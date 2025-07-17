557058d7b4d5be159a9cd38b7e48e692a9f95712
Harpalsinh Jadeja <jadejaharpal14@gmail.com>
2025-07-17 21:04:43 +0530

    feat: add `accountAssociation` for staging env

--- app/.well-known/farcaster.json/route.ts
+++ app/.well-known/farcaster.json/route.ts
@@ -3,7 +3,15 @@ import { APP_URL } from "../../../lib/constants";
 
 export async function GET() {
   const farcasterConfig = {
-    // TODO: Add account association
+    // TODO: Add your own account association
+    accountAssociation: {
+      header:
+        "eyJmaWQiOjE3OTc5LCJ0eXBlIjoiY3VzdG9keSIsImtleSI6IjB4MGMxNWE5QkVmRTg3RjY0N0IwMDNhMjI0MTY4NDYwMzYyODQ0M2Y4YiJ9",
+      payload:
+        "eyJkb21haW4iOiJtb25hZC1taW5pYXBwLXRlbXBsYXRlLWVudi1zdGFnaW5nLW1vbmFkLXh5ei52ZXJjZWwuYXBwIn0",
+      signature:
+        "MHgwNDI1N2ZiNjE4NjkwYjBkNDc3YzhiOGVjMzRlZTc2NWJmM2NhZjQzNzMxYWFhNDkzZGM3MmEyMjYxM2FkNDY1NDgyODg0MjRkM2E3Yjc4YTE2MzViNTM0NmQxZTA2YmZiZDUzOTZkYmRkNWY5MTc0OGExZDcxYTlhNTdjMjkwNDFi",
+    },
     frame: {
       version: "1",
       name: "Monad Farcaster MiniApp Template",
