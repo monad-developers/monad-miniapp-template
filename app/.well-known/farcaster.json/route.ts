1ae785e6bc55f2e03323efa7dc64b70a29844d12
Harpalsinh Jadeja <jadejaharpal14@gmail.com>
2025-08-04 21:24:37 +0530

    chore: remove `accountAssociation`

--- app/.well-known/farcaster.json/route.ts
+++ app/.well-known/farcaster.json/route.ts
@@ -4,14 +4,6 @@ import { APP_URL } from "../../../lib/constants";
 export async function GET() {
   const farcasterConfig = {
     // TODO: Add your own account association
-    accountAssociation: {
-      header:
-        "eyJmaWQiOjE3OTc5LCJ0eXBlIjoiYXV0aCIsImtleSI6IjB4N2Y3NDhmMTU0QjZEMTgwRDM1ZkExMjQ2MEM3RTRDNjMxZTI4QTlkNyJ9",
-      payload:
-        "eyJkb21haW4iOiJtb25hZC1taW5pYXBwLXRlbXBsYXRlLWVudi1zdGFnaW5nLW1vbmFkLXh5ei52ZXJjZWwuYXBwIn0",
-      signature:
-        "qWVx3t0XwZH3ljS4wXJH8pxPKDE4har3wy5r9xzKXA5oahOUeSlCxLNHEQDc5/PVi2QOOKxldihRUrowzHypwhs=",
-    },
     frame: {
       version: "1",
       name: "Monad Farcaster MiniApp Template",
